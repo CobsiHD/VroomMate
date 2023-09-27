bf95c5cf1e0becd0936922bd87fa8ec542fd111a
alexvelly <alexvelly007@gmail.com>
2023-09-27 14:55:29 +0200

    test2

--- app/src/main/java/com/example/vroommate/MainActivity.kt
+++ app/src/main/java/com/example/vroommate/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.vroommate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//WTFWTF
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
