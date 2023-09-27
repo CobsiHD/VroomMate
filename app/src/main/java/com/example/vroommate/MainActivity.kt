6a3521ee31416b49e9207e3177fe12790e412bab
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2023-09-27 16:08:54 +0200

    header v1

--- app/src/main/java/com/example/vroommate/MainActivity.kt
+++ app/src/main/java/com/example/vroommate/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.vroommate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//WTFWTF
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/vroommate/ProfilPage.kt
@@ -0,0 +1,4 @@
+package com.example.vroommate
+
+class ProfilPage {
+}
