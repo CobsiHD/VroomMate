e3f9cf32156f4e9d82e4c6f13dccff57c478bfc8
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2023-10-03 10:34:52 +0200

    push partie profil 1

--- app/src/main/java/com/example/vroommate/MainActivity.kt
+++ app/src/main/java/com/example/vroommate/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.vroommate
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
-//WTFWTF
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_main)
+
+        // Lier le bouton de profil à la ProfileActivity avec un Intent
+        val btnProfile: ImageButton = findViewById(R.id.btn_profil)
+        btnProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/vroommate/ProfilActivity.kt
+++ app/src/main/java/com/example/vroommate/ProfilActivity.kt
@@ -1,73 +1,67 @@
 package com.example.vroommate
 
-import android.app.Activity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
-import android.graphics.Bitmap
-import android.provider.MediaStore
-import android.content.Intent
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 
-class ProfileActivity : Activity() {
+class ProfileActivity : AppCompatActivity() {
 
-    private lateinit var editTextName: EditText
-    private lateinit var editTextSurname: EditText
+
+    private lateinit var editTextBrand: EditText
+    private lateinit var editTextModel: EditText
+    private lateinit var editTextEngine: EditText
+    private lateinit var editTextPower: EditText
+    private lateinit var editTextCouple: EditText
+    private lateinit var editText0100: EditText
+    private lateinit var editTextRPM: EditText
+    private lateinit var editTextSpeed: EditText
+
+
+    private lateinit var imageViewProfile: ImageView
     private lateinit var imageViewCar: ImageView
-    private lateinit var editTextCarSpec: EditText
 
-    // Initialisation des contrats pour la prise de photo ou le choix dans la galerie
-    private val takePhoto = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
-        // Mise à jour de l'imageView avec le bitmap de la photo prise
-        if (bitmap != null) {
-            imageViewCar.setImageBitmap(bitmap)
-        }
-    }
-
-    private val choosePhoto = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
-        // Mise à jour de l'imageView avec l'image choisie
-        imageViewCar.setImageURI(uri)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_profil) // Assurez-vous que le nom du layout est correct
 
-        editTextName = findViewById(R.id.editTextName)
-        editTextSurname = findViewById(R.id.editTextSurname)
+        // Initialise views
+        editTextBrand = findViewById(R.id.editTextBrand)
+        editTextModel = findViewById(R.id.editTextModel)
+        editTextEngine = findViewById(R.id.editTextEngine)
+        editTextPower = findViewById(R.id.editTextPower)
+        editTextCouple= findViewById(R.id.editTextCouple)
+        editText0100= findViewById(R.id.editText0100)
+        editTextRPM= findViewById(R.id.editTextRPM)
+        editTextSpeed= findViewById(R.id.editTextSpeed)
+
+        imageViewProfile = findViewById(R.id.imageViewProfile)
         imageViewCar = findViewById(R.id.imageViewCar)
-        editTextCarSpec = findViewById(R.id.editTextCarSpec)
 
-        findViewById<Button>(R.id.buttonChooseImage).setOnClickListener {
-            // Choix entre prendre une photo ou en choisir une depuis la galerie
-            showPictureDialog()
+
+        // Mise en place d'une action pour prendre une photo lorsque l'image est cliquée
+        val takePhoto = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap: Bitmap? ->
+            // Si la photo est prise avec succès (c.-à-d., bitmap non null), mettez à jour l'imageView
+            bitmap?.let { imageViewCar.setImageBitmap(it) }
         }
 
-        findViewById<Button>(R.id.buttonSaveProfile).setOnClickListener {
-            // Implémentez la fonctionnalité pour sauvegarder le profil
-        }
-    }
-
-
-
-
-
-    private fun showPictureDialog() {
-        //   capturer une photo avec la caméra
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        //  choisir une photo dans la galerie
-        val choosePictureIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        // Vérifier si l'appareil peut gérer la de prise de photo
-        val canTakePhoto: Boolean = takePictureIntent.resolveActivity(packageManager) != null
-        // Choisissez une action en fonction de la possibilité de prendre une photo ou non
-        if (canTakePhoto) {
-            //  pour prendre une photo
+        imageViewCar.setOnClickListener {
+            // Au clic, lance la capture d'une photo
             takePhoto.launch(null)
-        } else {
-            //   choisir une photo depuis la galerie
-            choosePhoto.launch("image/*")
+        }
+
+        imageViewProfile.setOnClickListener {
+            // Vous pouvez également ajouter une fonctionnalité pour changer la photo de profil ici
+        }
+
+        // Set click listener for Save Profile button
+        findViewById<Button>(R.id.buttonSaveProfile).setOnClickListener {
+            // Implement save profile functionality
+            // Exemple: sauvegarder les données dans une base de données ou les préférences partagées
         }
     }
 }
