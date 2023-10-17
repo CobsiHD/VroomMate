101b1d91403df899a84008d51c33e7716587fcae
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2023-10-17 16:58:54 +0200

    card

---
+++ app/src/main/java/com/example/vroommate/model/Car.kt
@@ -0,0 +1,14 @@
+package com.example.vroommate.model
+
+data class Car(
+    val model: String,
+    val engine: String,
+    val power: String,
+    val speed: String,
+    val topSpeed: String,
+    val rpm: String,
+    val torque: String,
+    val year: Int,
+    val imageResId: Int,
+    val flagResId: Int
+)
